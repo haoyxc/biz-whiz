299bf22428f60446a1e7a4ec9ce2d019f9dec589
haoyxc <48307028+haoyxc@users.noreply.github.com>
2019-07-30 12:15:35 -0400

    fixed dependencies

--- client/src/components/ArticleItem.js
+++ client/src/components/ArticleItem.js
@@ -3,9 +3,12 @@ import React from "react";
 export default function ArticleItem({ article }) {
   return (
     <div className="article">
-      <h5>{article.title}</h5>
-      <p>{article.description}</p>
-      <a href="{article.url}">More info here</a>
+      <h5 className="articleTitle">{article.title}</h5>
+      <p className="artcleDescription">{article.description}</p>
+      <p className="articleContent">{article.content}</p>
+      <a href="{article.url}" className="articleUrl">
+        {article.source.name}'s website
+      </a>
     </div>
   );
 }
